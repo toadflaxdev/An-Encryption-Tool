7b8ce3dbb5e273ea0f259ac13ba8c77b1a098261
toadflaxdev <61995452+toadflaxdev@users.noreply.github.com>
2020-03-10 11:56:52 +0000

    adding encrypt project
    first main commit for project

---
+++ NPS_Encryption/Main Menu Prototype/ConsoleEdits.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+class Console {
+
+public:
+
+	HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+
+	void hidecursor()
+	{
+		HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_CURSOR_INFO info;
+		info.dwSize = 100;
+		info.bVisible = FALSE;
+		SetConsoleCursorInfo(consoleHandle, &info);
+	}
+
+	void DisableResize()
+	{
+		/*
+		It only disable resizing by dragging the edge of windows. By the way, WS_SIZEBOX is the same as WS_THICKFRAME because of
+
+		#define WS_SIZEBOX WS_THICKFRAME
+		*/
+
+		HWND hWnd = GetConsoleWindow();
+		::SetWindowLong(hWnd, GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & ~WS_SIZEBOX);
+
+	}
+
+	void gameTitleWindow() {
+
+		SetConsoleTitle("NPS Encryption Tool");
+	}
+
+	void disableMaximize() {
+
+		// https://stackoverflow.com/questions/46145256/disable-maximize-button-c-console-application
+
+		HWND hwnd = GetConsoleWindow();
+		DWORD style = GetWindowLong(hwnd, GWL_STYLE);
+		style &= ~WS_MAXIMIZEBOX;
+		SetWindowLong(hwnd, GWL_STYLE, style);
+		SetWindowPos(hwnd, NULL, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_FRAMECHANGED);
+	}
+
+	void disableHighlighting() {
+		DWORD prev_mode;
+		GetConsoleMode(hInput, &prev_mode);
+		SetConsoleMode(hInput, ENABLE_EXTENDED_FLAGS |
+			(prev_mode & ~ENABLE_QUICK_EDIT_MODE));
+	}
+
+private:
+
+
+};
+
